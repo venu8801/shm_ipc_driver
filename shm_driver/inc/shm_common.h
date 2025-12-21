2b44dc5036615182fb1d338b8b1fd1b34f966715
Venu Gopal Atchyutanna <venu.ark.prasad@gmail.com>
2025-12-21 13:00:37 +0530

    shm_driver: Added driver registration and cdev init
    Signed-off-by: Venu Gopal Atchyutanna <venu.ark.prasad@gmail.com>

--- shm_driver/inc/shm_common.h
+++ shm_driver/inc/shm_common.h
@@ -6,6 +6,19 @@
  * 
  */
 
+
+#define DEVICE_MINOR_BEGIN 0
+#define DEVICES_NUM 1
+
+#define DRV_NAME "shm_driver_venus"
+
+typedef struct device_info {
+    dev_t dev_num;
+    unsigned int dev_minor_start;
+    unsigned int num_devs;
+    struct cdev shm_cdev;
+}devInfo;
+
 ssize_t shm_drv_read(struct file *, char __user *, size_t, loff_t *);
 ssize_t shm_drv_write(struct file *, const char __user *, size_t, loff_t *);
 int shm_drv_open(struct inode *, struct file *);
--- shm_driver/src/shm_module.c
+++ shm_driver/src/shm_module.c
@@ -2,12 +2,19 @@
  * This is a kernel module to implement shared memory IPC
  */
 
-#include <linux/kernel.h>
+#include <linux/cdev.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
-#include <shm_utils.h>
-#include <shm_error.h>
 #include <shm_common.h>
+#include <shm_error.h>
+#include <shm_utils.h>
+
+devInfo shm_drv_ctxt = {
+    .dev_num = 0,
+    .dev_minor_start = DEVICE_MINOR_BEGIN,
+    .num_devices = DEVICES_NUM,
+};
 /* shm_drv_ops routines */
 struct file_operations shm_fops = {
     .open = shm_drv_open,
@@ -23,34 +30,57 @@ int shm_drv_open(struct inode *filenode, struct file *f_object) {
     return SHM_SUCCESS;
 }
 
-ssize_t shm_drv_read(struct file *f_object, char __user * usr_buff, size_t usr_bsize, loff_t * usr_ofst) {
+ssize_t shm_drv_read(struct file *f_object, char __user *usr_buff, size_t usr_bsize, loff_t *usr_ofst) {
     printk(KERN_INFO "Shm driver read triggered");
     return SHM_SUCCESS;
 }
 
-ssize_t shm_drv_write(struct file *f_object, const char __user *usr_buff, size_t usr_bsize, loff_t *usr_ofst)
-{
+ssize_t shm_drv_write(struct file *f_object, const char __user *usr_buff, size_t usr_bsize, loff_t *usr_ofst) {
     printk(KERN_INFO "Shm driver write triggered\n");
     return SHM_SUCCESS;
 }
 
-int shm_drv_release(struct inode *file_inode, struct file *f_object)
-{
+int shm_drv_release(struct inode *file_inode, struct file *f_object) {
     printk(KERN_INFO "Shm driver relased\n");
     return SHM_SUCCESS;
 }
 
-long shm_drv_ioctl(struct file *f_object, unsigned int shm_cmd_id, unsigned long shm_arg){
+long shm_drv_ioctl(struct file *f_object, unsigned int shm_cmd_id, unsigned long shm_arg) {
     printk(KERN_INFO "Shm driver IOCTL cmd-id: [%d] \n", shm_cmd_id);
     return SHM_SUCCESS;
 }
 static int __init shm_init(void) {
     printk(KERN_INFO "shm init triggered\n");
-    return SHM_SUCCESS;
+    int ret = alloc_chrdev_region(&shm_drv_ctxt.dev_num, shm_drv_ctxt.dev_minor_start, shm_drv_ctxt.num_devs, DRV_NAME);
+    if (ret) {
+        printk(KERN_INFO "alloc_chrdev_region failed ret: %d\n", ret);
+        goto exit;
+    }
+    printk(KERN_INFO,
+           "char dev allocation successful Major: %d - Minor begin: %d - num "
+           "devices: %d\n",
+           MAJOR(shm_drv_ctxt.dev_num), MINOR(shm_drv_ctxt.dev_num), shm_drv_ctxt.num_devs);
+
+    // initialize cdev
+    cdev_init(&shm_drv_ctxt.shm_cdev, &shm_fops);
+
+    ret = cdev_add(&shm_drv_ctxt.shm_cdev, shrm_drv_ctxt.dev_num, shm_drv_ctxt.num_devs);
+    if (ret) {
+        printk(KERN_INFO "cdev_add failed ret: %d\n", ret);
+        goto unregister_chrdev;
+    }
+
+exit: 
+    return ret;
+unregister_chrdev:
+    unregister_chrdev_region(shm_drv_ctxt.dev_num, shm_drv_ctxt.num_devs);
+    return ret;
 }
 
 void __exit shm_deinit(void) {
     printk(KERN_INFO "shm de-init triggered\n");
+    unregister_chrdev_region(shm_drv_ctxt.dev_num, shm_drv_ctxt.num_devs);
+    cdev_del(&shm_drv_ctxt.shm_cdev);
     return;
 }
 
